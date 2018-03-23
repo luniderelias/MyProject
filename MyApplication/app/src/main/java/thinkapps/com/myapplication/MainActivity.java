fe5b4589d31dfc9e629576347dc4f863042f3892
Unknown <luniderelias@gtel.ufc.br>
2018-03-22 21:23:07 -0300

    Android annotations added

---
+++ MyApplication/app/src/main/java/thinkapps/com/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package thinkapps.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+    @AfterViews
+    void afterViews() {
+
+    }
+}
